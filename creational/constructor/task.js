61437b03a11284160aeb4bfa89595ac899f8fad4
Jane Fong <janeefongg@gmail.com>
2016-07-22 09:01:25 -0700

    [refactor] use es6 class

--- creational/constructor/task.js
+++ creational/constructor/task.js
@@ -1,31 +1,20 @@
 'use strict'
 
-var Task = function (name) {
-  this.name = name;
-  this.compvared = false;
+class Task {
+  constructor(name) {
+    this.name = name;
+    this.completed = false;
+  }
 
-  /*
-   Keep in mind:
-   Writing methods out is a drawback and not efficient because every time
-   Task gets instantiated, the method gets created again.
+  // this replaces writing it on the prototype
+  complete() {
+    console.log('Completing task: ' + this.name);
+    this.completed = true;
+  }
 
-   Ex:
-   this.save = function () {
-    console.log('Saving task: ' + this.name)
-   }
-
-   Use prototypes.
-   */
-
-}
-
-Task.prototype.complete = function () {
-  console.log('Completing task: ' + this.name);
-  this.completed = true;
-}
-
-Task.prototype.save = function () {
-  console.log('Saving task: ' + this.name)
+  save() {
+    console.log('Saving task: ' + this.name);
+  }
 }
 
 module.exports = Task;
