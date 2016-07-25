147c36a71fb153f90dc86d792fc3f64767ff1d9e
Jane Fong <janeefongg@gmail.com>
2016-07-25 16:15:11 -0700

    [new] mediator pattern example

--- behavioral/mediator/main.js
+++ behavioral/mediator/main.js
@@ -24,6 +24,41 @@ const auditingService = function () {
   }
 };
 
+let mediator = (function() {
+  let channels = {};
+
+  let subscribe = function (channel, context, func) {
+    if (!mediator.channels[channel]) {
+      mediator.channels[channel] = [];
+    }
+
+    mediator.channels[channel].push({
+      context: context,
+      func: func
+    });
+  };
+
+  let publish = function (channel) {
+    if (!this.channels[channel]) {
+      return false;
+    }
+    
+    let args = Array.prototype.slice.call(arguments, 1);
+
+    for (var i = 0; i < mediator.channels[channel].length; i++) {
+      let sub = mediator.channels[channel][i];
+      sub.func.apply(sub.context, args);
+    }
+  };
+
+  return {
+    channels: {},
+    subscribe: subscribe,
+    publish: publish
+  };
+
+})();
+
 const task1 = new Task({
   name: 'Demo',
   user: 'Jane'
@@ -32,3 +67,15 @@ const task1 = new Task({
 const notify = new notificationService();
 const log = new loggingService();
 const audit = new auditingService();
+
+
+mediator.subscribe('complete', notify, notify.update);
+mediator.subscribe('complete', log, log.update);
+mediator.subscribe('complete', audit, audit.update);
+
+task1.complete = function () {
+  mediator.publish('complete', this);
+  Task.prototype.complete.call(this);
+};
+
+task1.complete();
