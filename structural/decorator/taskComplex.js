d4a25767531179e97faa1307310d2a80020d30ba
Jane Fong <janeefongg@gmail.com>
2016-07-24 18:17:29 -0700

    [new] more complex decorator pattern

---
+++ structural/decorator/taskComplex.js
@@ -0,0 +1,44 @@
+'use strict'
+
+class Task {
+  constructor(name) {
+    this.name = name;
+    this.completed = false;
+  }
+
+  complete() {
+    console.log('Completing task: ' + this.name);
+    this.completed = true;
+  }
+
+  save() {
+    console.log('Saving task: ' + this.name);
+  }
+}
+
+const myTask = new Task('Legacy Task');
+myTask.complete();
+myTask.save();
+
+//sub-class
+class UrgentTask extends Task {
+  constructor(name, priority) {
+    super(name);
+    this.priority = priority;
+  }
+
+  notify() {
+    console.log('Notifying important people');
+  }
+
+  save() {
+    console.log('Do something special before saving');
+    //same as Task.prototype.save.call(this)
+    super.save();
+  }
+};
+
+const ut = new UrgentTask('This is urgent', 1);
+ut.complete();
+ut.notify();
+ut.save();
